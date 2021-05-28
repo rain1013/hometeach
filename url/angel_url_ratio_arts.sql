21afe66e6750710e5df6c3f54df77152fa7b29ef
rain1013 <66631188+rain1013@users.noreply.github.com>
2021-05-28 21:50:46 +0800

    update url sql

--- url/angel_url_ratio_arts.sql
+++ url/angel_url_ratio_arts.sql
@@ -5,7 +5,7 @@ with data1 as (
 		field3,
 		field1
 	from URL_Classification
-	where field2 REGEXP '(?<=)(angel)' and field3 = 'Arts'
+	where field2 REGEXP '(.co.uk)' and field3 = 'Arts'
 ),
 
 data2 as (
@@ -23,5 +23,4 @@ total_amt,
 cast(angel_url_amt as float)/cast(total_amt as float) as angel_ratio
 from data2
 join data1 on data1.ID = data2.ID
-
 
---
+++ url/kids_url_ratio.sql
@@ -0,0 +1,24 @@
+with data1 as (
+select 
+1 as ID,
+count(field2) as game_url_amt
+from URL_Classification
+where field3 = 'Kids' and 
+field2 REGEXP '(?<=)(xbox)+'
+),
+
+data2 as (
+	select
+		1 as ID,
+		count(field2) as total_amt,
+		field3
+	from URL_Classification
+	where field3  = 'Kids'
+)
+
+select 
+game_url_amt,
+total_amt,
+cast(game_url_amt as float)/cast(total_amt as float) as game_ratio
+from data2
+join data1 on data1.ID = data2.ID
