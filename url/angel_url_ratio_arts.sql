af0928d12e849ff5089970a642424db522031dfc
rain1013 <66631188+rain1013@users.noreply.github.com>
2021-05-28 13:26:45 +0800

    push url file and hw1

---
+++ url/angel_url_ratio_arts.sql
@@ -0,0 +1,27 @@
+with data1 as (
+	select 
+		1 as ID,
+		count(field2) as angel_url_amt,
+		field3,
+		field1
+	from URL_Classification
+	where field2 REGEXP '(?<=)(angel)' and field3 = 'Arts'
+),
+
+data2 as (
+	select
+		1 as ID,
+		count(field2) as total_amt,
+		field3
+	from URL_Classification
+	where field3  = 'Arts'
+)
+
+select 
+angel_url_amt,
+total_amt,
+cast(angel_url_amt as float)/cast(total_amt as float) as angel_ratio
+from data2
+join data1 on data1.ID = data2.ID
+
+
